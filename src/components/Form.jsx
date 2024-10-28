1925eeb04d17d1560a330782b7c312f12a0591ee
Felito802 <felitorrijos@gmail.com>
2024-10-27 22:27:34 -0500

    ajuste apuntador

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ function Form({callback}){
     const validateUser = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch('http://localhost:4000/v1/signos/login', {
+            const response = await fetch('https://parcial-back.vercel.app/?vercelToolbarCode=l-Jmf8e9sUUmtKX/v1/signos/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
